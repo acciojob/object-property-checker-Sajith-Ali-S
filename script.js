854589790ab4de37dd62fa3a90f9aa3739f38f19
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-04-23 09:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-let keys = Object.keys(sampleObject);
-function hasKey(key) {
-  for (let i in keys) {
-  	 if(keys[i]==key)return true
-  }
-	return false
-	
-}
 
+function hasKey(key){
+	for(let i in sampleObject){
+		if(key==i){
+			return true
+		}
+	}
+	return false
+}
 // Do not change the code below
 const key = prompt("Enter Key.");
-alert(hasKey(key)); 
+alert(hasKey(key)); //
